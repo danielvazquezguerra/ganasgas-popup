8018078d3310aa94ec924f1557188eafbd6ccf3e
dmartinez <dmartinez@ganaenergia.com>
2020-09-15 17:12:17 +0200

    funcion fuera de pantalla iniciada

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.scss';
 import Popup from './components/Popup/Popup';
 
 function App() {
+
+  const getPopup = () => {
+    console.log('Estas fuera de pantalla');
+  }
+
   return (
-    <div className="App">
+    <div className="App" onMouseLeave={getPopup}>
 
         <h1>Popup</h1>
         <div id="wrapper" className="Wrapper">
-          <p><a class="button" href="#popup1">Click Me</a></p>
+          <p><a className="button" onClick={getPopup} href="#popup1">Click Me</a></p>
         </div>
 
         <Popup />
--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import './Popup.scss';
 
 const Popup = () => {
-  return (
-    <div id="popup1" class="overlay">
-        <div class="popup">
-        
-            <div class="wrapper-up">
 
-                <a class="close" href="#">&times;</a>
+    
+  return (
+    <div id="popup1" className="overlay">
+
+        <div className="popup">
+        
+            <div className="wrapper-up">
+
+                <a className="close" href="#">&times;</a>
 
             </div>
 
-            <div class="wrapper-down">
+            <div className="wrapper-down">
                 
-                <button class="bt-tarifas">Ver tarifas</button>
+                <button className="bt-tarifas">Ver tarifas</button>
 
             </div>
 
